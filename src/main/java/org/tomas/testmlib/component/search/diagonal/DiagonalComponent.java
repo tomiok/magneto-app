c7eb1ad41bdf9ed8f40f6649867381fa6e6e2239
Tomas Lingotti <tomas.lingotti@globant.com>
2018-01-13 19:07:03 -0300

    Refactor in javadocs

--- src/main/java/org/tomas/testmlib/component/search/diagonal/DiagonalComponent.java
+++ src/main/java/org/tomas/testmlib/component/search/diagonal/DiagonalComponent.java
@@ -3,9 +3,8 @@ package org.tomas.testmlib.component.search.diagonal;
 public interface DiagonalComponent {
 
   /**
-   *
    * @param dna The person DNA.
-   * @return Number of sequences found in the given DNA.
+   * @return the number of sequences that match with the pattern.
    */
   long countMutantSequencesInDiagonals(final String[] dna);
 }
--- src/main/java/org/tomas/testmlib/component/search/row/RowComponent.java
+++ src/main/java/org/tomas/testmlib/component/search/row/RowComponent.java
@@ -3,9 +3,8 @@ package org.tomas.testmlib.component.search.row;
 public interface RowComponent {
 
   /**
-   *
-   * @param dna The DNA of the person
-   * @return The number of sequences which have more than 4 repeated letters.
+   * @param dna The person DNA.
+   * @return the number of sequences that match with the pattern.
    */
   long countMutantSequencesInRows(final String[] dna);
 }
